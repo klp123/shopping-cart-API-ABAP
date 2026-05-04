260c0b9ddce387918135dd3212a2d8da58e72f3f
klp123 <praneethkurapati8@gmail.com>
2026-05-04 15:49:45 +0530

    update axios

--- src/services/odataService.js
+++ src/services/odataService.js
@@ -1,6 +1,6 @@
 
 const axios = require("axios");
-const BASE_URL = "http://localhost:4000/odata";
+const BASE_URL = "http://23.474.3227.22:4000/odata";
 
 async function fetchProductsFromOData(query) {
   const filters = [];
@@ -14,11 +14,12 @@ async function fetchProductsFromOData(query) {
   if (query && query.limit) params['$top']    = query.limit;
   if (query && query.skip)  params['$skip']   = query.skip;
 
-  const res = await axios.get(`${BASE_URL}/Products`, { params });
-  return {
-    count: res.data['@odata.count'],
-    products: res.data.value,
-  };
+  // const res =
+  // await axios.get(`${BASE_URL}/Products`, { params });
+  // return {
+  //   count: res.data['@odata.count'],
+  //   products: res.data.value,
+  // };
 }
 
 module.exports = { fetchProductsFromOData };
